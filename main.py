16f94dc57bf165c464f35d26c59c7b44fbe15907
DAMIEN SINGH <dsingh005@citymail.cuny.edu>
2020-08-05 13:11:02 -0400

    ..

--- main.py
+++ main.py
@@ -22,13 +22,14 @@ class Node:
         self.x = x
         self.y = y
         self.wall = False
+        self.body = pygame.Rect(self.x, self.y, CELLSIZE, CELLSIZE)
 
     def drawNODE(self, surface):
         i = self.x * CELLSIZE + XMARGIN
         j = self.y * CELLSIZE + YMARGIN
         if self.wall:
             pygame.draw.rect(surface, Black, (i, j, CELLSIZE, CELLSIZE))
-        else:
+        elif not self.wall:
             pygame.draw.rect(surface, White, (i, j, CELLSIZE, CELLSIZE))
 
 
@@ -49,6 +50,11 @@ class Board:
             for j in range(self.cols):
                 self.grid[i][j].drawNODE(surface)
 
+        for x in range(XMARGIN, SCREENWIDTH, CELLSIZE):
+            pygame.draw.line(surface, Black, (x, 0), (x, SCREENWIDTH))
+        for y in range(YMARGIN, SCREENHEIGHT, CELLSIZE):
+            pygame.draw.line(surface, Black, (0, y), (SCREENWIDTH, y))
+
 
 def leftTopofTile(a, b):
     left = XMARGIN + (a * CELLSIZE) + (a - 1)
@@ -61,12 +67,24 @@ def display(surface):
     left, top = leftTopofTile(0, 0)
     width = COLS * CELLSIZE
     height = ROWS * CELLSIZE
-    pygame.draw.rect(surface, White, (left, top, width, height), 2)
-
+    pygame.draw.rect(surface, BLUE, (left - 5, top - 5, width + 11, height + 10), 5)
     grid = Board(ROWS, COLS)
     grid.drawGRID(surface)
 
+
+def getMouseClick(surface, xpos, ypos):
+    global grid
+    for i in range(SCREENWIDTH):
+        for j in range(SCREENHEIGHT):
+            left, top = leftTopofTile(xpos, ypos)
+            tileSelector = pygame.Rect(left, top, CELLSIZE, CELLSIZE)
+            if tileSelector.collidepoint(xpos, ypos):
+                return(xpos, ypos)
+    return (None, None)
+
+
 def main():
+    grid = Board(ROWS, COLS)
     pygame.init()
     myWindow = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
     pygame.display.set_caption("Visualizer")
@@ -76,6 +94,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                xpos, ypos = getMouseClick(myWindow, event.pos[0], event.pos[1])
+                for i in range(grid.rows):
+                    for j in range(grid.cols):
+                        if grid.grid[i][j].body.collidepoint(event.pos):
+                            print(grid.grid[i][j])
+                            grid.grid[i][j].wall = True
+
+
+
+
 
         display(myWindow)
         pygame.display.update()
