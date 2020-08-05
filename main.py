bfe792067348ec3c927827c1e5f052bd193deb0c
DAMIEN SINGH <dsingh005@citymail.cuny.edu>
2020-08-05 12:10:09 -0400

    added borders

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
-SCREENWIDTH = 800
-SCREENHEIGHT = 800
+SCREENWIDTH = 1000
+SCREENHEIGHT = 1000
 BORDERWIDTH = 600
 BORDERHEIGHT = 600
 CELLSIZE = 40
@@ -37,8 +37,6 @@ class Board:
         self.rows = ROWS
         self.cols = COLS
         self.grid = []
-
-    def drawGRID(self, surface):
         for i in range(self.rows):
             self.column = []
             for j in range(self.cols):
@@ -46,10 +44,27 @@ class Board:
                 self.column.append(node)
             self.grid.append(self.column)
 
+    def drawGRID(self, surface):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.grid[i][j].drawNODE(surface)
+
+
+def leftTopofTile(a, b):
+    left = XMARGIN + (a * CELLSIZE) + (a - 1)
+    top = YMARGIN + (b * CELLSIZE) + (b - 1)
+    return(left, top)
+
 
 def display(surface):
-    surface.fill(BLUE)
+    surface.fill(Black)
+    left, top = leftTopofTile(0, 0)
+    width = COLS * CELLSIZE
+    height = ROWS * CELLSIZE
+    pygame.draw.rect(surface, White, (left, top, width, height), 2)
 
+    grid = Board(ROWS, COLS)
+    grid.drawGRID(surface)
 
 def main():
     pygame.init()
@@ -61,7 +76,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
         display(myWindow)
+        pygame.display.update()
 
 
 if __name__ == '__main__':
